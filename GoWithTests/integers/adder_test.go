e70a929960a636fb988ee658919123f66ce3303e
Reza-sd <136134769+Reza-sd@users.noreply.github.com>
2023-12-29 01:08:16 +0000

    adder_test

--- GoWithTests/integers/adder_test.go
+++ GoWithTests/integers/adder_test.go
@@ -1,13 +1,22 @@
 package integers
 
-import "testing"
+import (
+	//"fmt"
+	"testing"
+)
 
 // =========================================================
 func Test_Add(t *testing.T) {
 	//----------
-	t.Run("1-adding two integers", func(t *testing.T) {
+	t.Run("1-adding two zero", func(t *testing.T) {
+		act := Add(0, 0)                //Act - my act
+		exp := 0                        //Expected - my expectation
+		assertCorrectValue(t, act, exp) //assertion - my assertion
+	})
+	//-----------
+	t.Run("2-adding two integers", func(t *testing.T) {
 		act := Add(1, 2)                //Act - my act
-		exp := 3                        //Expected - my expectation
+		exp := 30                        //Expected - my expectation
 		assertCorrectValue(t, act, exp) //assertion - my assertion
 	})
 	//-----------
@@ -16,6 +25,7 @@ func Test_Add(t *testing.T) {
 // =============================================
 func assertCorrectValue(t testing.TB, act, exp int) {
 	t.Helper()
+	//fmt.Println("---> act=", act, "-- exp=", exp)
 	if act != exp {
 		t.Errorf("got %q want %q", act, exp)
 	}
