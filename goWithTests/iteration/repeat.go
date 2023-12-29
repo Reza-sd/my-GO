a5f919fe562702a094776b7d89874d6410224ee4
Reza-sd <136134769+Reza-sd@users.noreply.github.com>
2023-12-29 02:14:29 +0000

    iteration 2

--- goWithTests/iteration/repeat.go
+++ goWithTests/iteration/repeat.go
@@ -2,10 +2,8 @@ package iteration
 
 func Repeat(character string, iteration int) string {
 	result := ""
-
 	for i := 0; i < iteration; i++ {
 		result += character
 	}
-
 	return result
 }
--- goWithTests/iteration/repeat_test.go
+++ goWithTests/iteration/repeat_test.go
@@ -23,6 +23,12 @@ func Test_Add(t *testing.T) {
 		assertCorrectValue(t, act, exp) //assertion - my assertion
 	})
 	//-----------
+	t.Run("t4-twe chatacter to 3 repeat", func(t *testing.T) {
+		act := Repeat("ab", 3)          //Act - my act
+		exp := "ababab"                 //Expected - my expectation
+		assertCorrectValue(t, act, exp) //assertion - my assertion
+	})
+	//-----------
 }
 
 // ==============assertion-function======================
@@ -35,19 +41,19 @@ func assertCorrectValue(t testing.TB, act, exp string) {
 
 // =================Benchmark=========================
 func Benchmark_Repeat(b *testing.B) {
-	b.Run("b1-one chatacter to 5 repeat", func(b *testing.B) {
+	b.Run("b1-one chatacter to 10 repeat", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
 			Repeat("a", 10)
 		}
 	})
 	//-----------
-	b.Run("b2-two chatacter to 5 repeat", func(b *testing.B) {
+	b.Run("b2-two chatacter to 15 repeat", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
 			Repeat("bc", 15)
 		}
 	})
 	//-----------
-	b.Run("b3-three chatacter to 5 repeat", func(b *testing.B) {
+	b.Run("b3-three chatacter to 20 repeat", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
 			Repeat("e2f", 20)
 		}
