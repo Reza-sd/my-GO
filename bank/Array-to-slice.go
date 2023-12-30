eac39d7f828e387767f2a7e1b359c71869ede4fb
Reza-sd <136134769+Reza-sd@users.noreply.github.com>
2023-12-30 17:47:00 +0000

    array-to-slice

---
+++ bank/Array-to-slice.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+  "fmt"
+)
+
+func main() {
+  x := [3]string{"Лайка", "Белка", "Стрелка"}
+
+  y := x[:] // slice "y" points to the underlying array "x"
+
+  z := make([]string, len(x))
+  copy(z, x[:]) // slice "z" is a copy of the slice created from array "x"
+
+  y[1] = "Belka" // the value at index 1 is now "Belka" for both "y" and "x"
+
+  fmt.Printf("%T %v\n", x, x) //[3]string [Лайка Belka Стрелка]
+  fmt.Printf("%T %v\n", y, y) // []string [Лайка Belka Стрелка]
+  fmt.Printf("%T %v\n", z, z) //[]string [Лайка Белка Стрелка]
+
+}
