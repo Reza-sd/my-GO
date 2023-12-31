30b7d797c23e87298f0008f2ad3d9694f0f18ea8
Reza-sd <136134769+Reza-sd@users.noreply.github.com>
2023-12-31 03:02:32 +0000

    test-passed

--- gowithTests/Structs/shapes.go
+++ gowithTests/Structs/shapes.go
@@ -1,9 +1,20 @@
 package main
 
-func Perimeter(width float64, height float64) float64 {
-  return 2 * (width + height)
+//===========setup===========
+type Rectangle struct {
+  Width  float64
+  Height float64
+}
+//----------
+type Circle struct {
+  Radius float64
+}
+//----------
+
+func Perimeter(rectangle Rectangle) float64 {
+  return 2 * (rectangle.Width + rectangle.Height)
 }
 
-func Area(width float64, height float64) float64 {
-  return width * height
+func Area(rectangle Rectangle) float64 {
+  return rectangle.Width * rectangle.Height
 }
--- gowithTests/Structs/shapes_test.go
+++ gowithTests/Structs/shapes_test.go
@@ -8,22 +8,12 @@ import (
 func Test_Perimeter(t *testing.T) {
 	//----------
 	t.Run("t1-calculate the perimeter of a rectangle given a height and width", func(t *testing.T) {
-
-		act := Perimeter(10.0, 10.0)  //Act
+    rectangle1 := Rectangle{10.0, 10.0}
+		act := Perimeter(rectangle1)  //Act
 		exp := 40.0                   //Expectation
 		assert_Perimeter(t, act, exp) //Assertion
 	})
-}
-
-// =========================Test_Area=============================
-func Test_Area(t *testing.T) {
-	//----------
-	t.Run("t1-calculate the Area of a rectangle given a height and width", func(t *testing.T) {
-
-		act := Area(12.0, 6.0)        //Act
-		exp := 72.0                   //Expectation
-		assert_Perimeter(t, act, exp) //Assertion
-	})
+	//---------
 }
 
 // ----------------assert_Perimeter-----------------------
@@ -36,4 +26,26 @@ func assert_Perimeter(t testing.TB, act, exp float64) {
 	}
 }
 
-//----------------------------------------------
+// =========================Test_Area=============================
+func Test_Area(t *testing.T) {
+	//----------
+	t.Run("t1-calculate the Area of a rectangle given a height and width", func(t *testing.T) {
+    rectangle1 := Rectangle{12.0, 6.0}
+		act := Area(rectangle1)   //Act
+		exp := 72.0              //Expectation
+		assert_Area(t, act, exp) //Assertion
+	})
+	//-----------
+}
+
+// ------------------assert_Area-----------------
+func assert_Area(t testing.TB, act, exp float64) {
+	t.Helper()
+	if act != exp {
+		// Use %q for strings or specific verbs such as %d for integers and %f for float64.
+		//The f is for our float64 and the .2 means print 2 decimal places.
+		t.Errorf("---> got %.2f BUT expected %.2f <---", act, exp)
+	}
+}
+
+//=====================END===========================
