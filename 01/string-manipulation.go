c3d54baeed05d1f22811e43c39d03a4b175e38ab
Reza-sd <136134769+Reza-sd@users.noreply.github.com>
2024-01-10 11:18:15 +0000

    string-manipulation

---
+++ 01/string-manipulation.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+  str := "Hello"
+  //cast them first
+  strch := []byte(str)
+  strChRune :=[]rune(str)
+
+
+  fmt.Println("str=", str)
+  fmt.Println("strch(byte)=", strch)
+  fmt.Println("strChRune=", strChRune)
+  strch[0] = 'A'
+  fmt.Println("strch=", strch)
+  strFinal := string(strch)
+  fmt.Println("strFinal=", strFinal)
+}
+/*
+str= Hello
+strch(byte)= [72 101 108 108 111]
+strChRune= [72 101 108 108 111]
+strch= [65 101 108 108 111]
+strFinal= Aello
+*/
